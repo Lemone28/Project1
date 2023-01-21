49b6951d1b4a16ee6992a9943addac9d993c5e83
Lemone28 <123073262+Lemone28@users.noreply.github.com>
2023-01-21 18:01:14 +0200

    ver_2
    Файл script.js, 5 рядок зміна розміру шрифта з 20px на 30px/

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var elements = document.getElementsByTagName('p');
 
 for(var i = 0; i < elements.length; i++) {
     elements[i].style.color = "green";
-    elements[i].style.fontSize = "20px";
+    elements[i].style.fontSize = "30px";
     elements[i].style.textAlign = "center";
 }
